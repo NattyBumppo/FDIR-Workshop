614c9c36de352fcf1f8f7cace14bc418d2115f7e
Nicholas Reiter <nreiter@cs.washington.edu>
2015-06-07 19:57:55 -0700

    Adding deletion of plots. Also race conditions. Beware.

--- viz/static/js/graphs.js
+++ viz/static/js/graphs.js
@@ -16,10 +16,20 @@
       return false;
     }
 
+    var container = $(document.createElement('div'));
+    container.addClass('detail_chart_container');
+
+    var x_button = $(document.createElement('a'));
+    x_button.addClass('chart_close_button');
+    x_button.text('X');
+
     var div = $(document.createElement('div'));
     div.addClass('detail_chart');
 
-    graph_area.append(div);
+    container.append(x_button);
+    container.append(div);
+
+    graph_area.append(container);
 
     var chart = c3.generate(
       {
@@ -56,9 +66,13 @@
     graphs[channel] = {
       chart: chart,
       channel: channel,
-      last_loaded: timer.getTime() - 1,
-      container: div// Sometime in the 'past' to force update
+      last_loaded: timer.getTime() - 1
     };
+
+    // Add click handler to close it, now that we have the chart reference
+    container.data('channel', channel);// Could pass via the event.data, but binding here for now
+    x_button.click(closeChart);
+
   }
 
   graph.updateGraphs = function(time) {
@@ -67,20 +81,35 @@
     }
   }
 
-  function updateGraph(info, time) {
-    data_store.getData(
-      info.channel,
-      time,
-      generateDataCallback(info.chart)
-    );
+  function closeChart() {
+    var container = $(this).parent();
+    var chart = graphs[container.data('channel')].chart;
+    graphs[container.data('channel')] = undefined;
+    chart.destroy(); // There was a race condition here... O.o Needs to still
+                     // be looked into, to see if there is a guaranteed fix
+    container.remove();
   }
 
-  function generateDataCallback(chart) {
-    return function(info) {
-      chart.load(info.data);
+  function updateGraph(info, time) {
+    // Another race condition?!
+    if(info != undefined) {
+      data_store.getData(
+        info.channel,
+        time,
+        generateDataCallback(info.channel)
+      );
+    }
+  }
 
-      chart.axis.min(info.config.y.min);
-      chart.axis.max(info.config.y.max);
+  function generateDataCallback(channel) {
+    return function(info) {
+      if(graphs[channel] != undefined) {
+        var chart = graphs[channel].chart;
+        chart.load(info.data);
+
+        chart.axis.min(info.config.y.min);
+        chart.axis.max(info.config.y.max);
+      }
     }
   }
 
